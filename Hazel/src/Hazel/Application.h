e3b37f5ab93e0553ad8d9d95e9201d8d244c955b
liamdugg <liamdug@gmail.com>
2023-03-29 00:12:58 -0300

    Added ImGui layer/overlay test

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -22,6 +22,9 @@ namespace Hazel {
 
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* layer);
+
+		inline static Application& Get() { return *s_Instance; }
+		inline Window& GetWindow() { return *m_Window; }
 	
 	private:
 		
@@ -30,6 +33,9 @@ namespace Hazel {
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+	private:
+		static Application* s_Instance;
 	};
 
 	// To be defined in CLIENT
